1927e0031c6cf57fc9a444e013b8e718a00238cc
Pritom_Dey <sstudentinfo@gmail.com>
2021-11-24 16:38:39 +0600

    first-commit

---
+++ src/components/individualComponents/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../../sharedComponents/Navigation/Navigation";
+import Homebody from "./HomeBody/HomeBody";
+const Home = () => {
+  return (
+    <div>
+      <Navigation />
+      <Homebody />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/individualComponents/Home/HomeBody/HomeBody.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./HomeBody.css";
+const Homebody = () => {
+  return (
+    <div className="home-body">
+      <div className="overlay"></div>
+      <div className="home-body-content text-light">
+        <h3>Welcome to CSECU</h3>
+        <h1 className="header1">It's nice to meet you.</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Homebody;
