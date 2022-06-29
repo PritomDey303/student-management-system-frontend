315c03d132513f316f24ce8d795674a6608fcf37
Pritom Dey <pritomdey4@gmail.com>
2022-06-29 16:51:07 +0600

    start

--- src/components/individualComponents/Dashboard/Dashboard.js
+++ src/components/individualComponents/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import { ContextProvider } from "../../../App";
 import Footer from "../../sharedComponents/Footer/Footer";
 import Navigation from "../../sharedComponents/Navigation/Navigation";
